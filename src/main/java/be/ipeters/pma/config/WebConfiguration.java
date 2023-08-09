253c76af28b2421b32891cb907bbb501224bb7b5
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2023-08-09 14:25:29 +0200

    working with H2 in mem

--- src/main/java/be/ipeters/pma/config/WebConfiguration.java
+++ src/main/java/be/ipeters/pma/config/WebConfiguration.java
@@ -1,7 +1,6 @@
 package be.ipeters.pma.config;
 
 import org.h2.server.web.WebServlet;
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
